73b6edea753de3f28add1f462a55d942d0d2af21
Thiago Nascimento <thiago.nasciment8512@gmail.com>
2019-09-06 00:12:48 -0300

    Iniciando a estilização

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Image } from 'react-native';
 import PropTypes from 'prop-types';
 
 import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 import {
   Container,
@@ -18,8 +21,17 @@ import {
 export default function SignUp({ navigation }) {
   const emailRef = useRef();
   const passwordRef = useRef();
+  const dispatch = useDispatch();
 
-  function handleSubmit() {}
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const { loading } = useSelector(state => state.auth);
+
+  function handleSubmit() {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <Background>
@@ -33,6 +45,8 @@ export default function SignUp({ navigation }) {
             placeholder="Nome Completo"
             returnKeyType="next"
             onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
           />
 
           <FormInput
@@ -44,6 +58,8 @@ export default function SignUp({ navigation }) {
             ref={emailRef}
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
@@ -54,9 +70,13 @@ export default function SignUp({ navigation }) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Cadastrar
+          </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignIn')}>
---
+++ src/store/createStore.js
@@ -0,0 +1,11 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+
+export default (reducers, middlewares) => {
+  const enhancer = __DEV__
+    ? compose(
+        console.tron.createEnhancer(),
+        applyMiddleware(...middlewares)
+      )
+    : applyMiddleware();
+  return createStore(reducers, enhancer);
+};
